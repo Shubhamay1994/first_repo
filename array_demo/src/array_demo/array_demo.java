c2e756a2156c127ef84e83b15075e27a21a82855
shubhamay chand <shbhamaychand.ee@gmail.com>
2020-06-24 04:18:41 -0700

    add folder

---
+++ array_demo/src/array_demo/array_demo.java
@@ -0,0 +1,21 @@
+package array_demo;
+
+
+
+public class array_demo{
+	public static void main(String[] args) {
+		int num[][] = {{1,2,3,4},{3,4},{4,5,6,7,8,8,9}};
+		for(int k[]:num) {
+			for(int l:k) {
+				System.out.print(" "+l);
+			}
+			System.out.println( );
+		}
+			    
+			
+			
+	}
+		
+}
+		
+
